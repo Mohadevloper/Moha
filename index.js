bb2b32af1dd674295ace3b0a91c437077b65a84c
Mohadevloper <162472582+Mohadevloper@users.noreply.github.com>
2024-05-09 21:33:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,162 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const chalk = require("chalk");
+app.listen(() => console.log("Server started"));
 
-app.get('/', (req, res) => {
-        res.send('Hello Express app!')
-})
 app.use('/ping', (req, res) => {
-        res.send(new Date());
-});
-app.listen(3000, () => {
-        console.log(chalk.red.bold('The Bot Is Ready.'))
+  res.send(new Date());
 });
 
-const Discord = require("discord.js");
+//كود تثبيت حسابك ٢٤ ساعة في روم فويس
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client({checkUpdate: false,}); // All partials are loaded automatically
 
-const { Client , MessageActionRow , MessageButton , MessageEmbed , MessageSelectMenu , Intents } = require("discord.js");
-
-
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_BANS,
-        Intents.FLAGS.GUILD_INTEGRATIONS,
-        Intents.FLAGS.GUILD_WEBHOOKS,
-        Intents.FLAGS.GUILD_INVITES,
-        Intents.FLAGS.GUILD_VOICE_STATES,
-        Intents.FLAGS.GUILD_PRESENCES,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.GUILD_MESSAGE_TYPING,
-        Intents.FLAGS.DIRECT_MESSAGES,
-        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
-    ],
- ws: {properties: { $browser:"Discord iOS" }
-      },
-});
-
-const db = require("quick.db");
-const ms = require("ms");
-
-const prefix = "!";
-
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+//ثبات فويس 24 ساعه v13 بدون اي مشاكل
+const { joinVoiceChannel } = require('@discordjs/voice');
 client.on('ready', () => {
-  console.log(`================`)
-  console.log(``)
-  console.log(`Bot User name : ${client.user.username}`)
-  console.log(`Bot Id : ${client.user.id}`)
-  console.log(`Bot Tag : ${client.user.tag}`)
-  console.log(`Servers Count : ${client.guilds.cache.size}`)
-  console.log(`Users Count : ${client.guilds.cache
-.reduce((a, b) => a + b.memberCount, 0)
-.toLocaleString()}`)
-  console.log(``)
-  console.log(`================`)
- client.user.setActivity(`${prefix}help`,  { type: 'online' })  
-});
-client.login(process.env.token).catch((err) => {
-   console.log(err.message)
-});
-
-// ========================== Const ======================== \\
-
-const TaxChannel = "1212726505147334686" // ايدي الات شانل الاوتو تاكس
-
-// ==================== Auto Tax ====================== \\
-
-client.on("messageCreate", message => {
- if(message.channel.type === "dm" || 
-  message.author.bot) return
-  
-if(TaxChannel.includes(message.channel.id)){
-
-  var args = message.content.split(' ').slice(0).join(' ')
-if(!args) return;
-  
-if (args.endsWith("m")) args = args.replace(/m/gi, "") * 1000000;
-else if (args.endsWith("k")) args = args.replace(/k/gi, "") * 1000;
-else if (args.endsWith("K")) args = args.replace(/K/gi, "") * 1000;
-else if (args.endsWith("M")) args = args.replace(/M/gi, "") * 1000000;
-     let args2 = parseInt(args)
-    let tax = Math.floor(args2 * (20) / (19) + (1))
-    let tax2 = Math.floor(args2 * (20) / (19) + (1) - (args2))
-    let tax3 = Math.floor(tax2 * (20) / (19) + (1))
-    let tax4 = Math.floor(tax2 + tax3 + args2)
-      
-    let Taxembed = new MessageEmbed()
-      
-.setAuthor(`TAX CALCULATOR`, client.user.avatarURL({ dynamic: true }))
-
-   .setThumbnail(client.user.avatarURL({ dynamic: true }))   
-      
-.addFields([
-    {
-    name: `الضريبه: `,
-    value: `${tax}`
-},
-  {
-    name: `ضريبة الوسيط: `, 
-    value: `${tax4}`
-},
-])
-        .setColor(message.guild.me.displayColor)
-  .setTimestamp()
-message.reply({embeds: [Taxembed]}).catch((err) => {
-   console.log(err.message)
-   });    
-  }
+    
+    setInterval( async () => {
+    client.channels.fetch("1216400197593206926") 
+     .then((channel) => { 
+      const VoiceConnection = joinVoiceChannel({
+       channelId: channel.id, 
+       guildId: channel.guild.id, 
+       adapterCreator: channel.guild.voiceAdapterCreator 
+       });
+    }).catch((error) => { return; });
+    }, 1000)
 }); 
 
-// ================= Cmd Tax ================ \\
 
-client.on("messageCreate", async message => {
-    if (message.author.bot) return;
-  if (!message.guild) return;
-  if (message.content.toLowerCase().startsWith(prefix + "tax".toLowerCase())) { 
-  let args = message.content
-    .split(" ")
-    .slice(1)
-    .join(" "); 
-    if(!args) return message.reply("**:rolling_eyes: Please enter a number**").catch((err) => {
-   console.log(err.message)
-   });
-    
-if (args.endsWith("m")) args = args.replace(/m/gi, "") * 1000000;
-else if (args.endsWith("k")) args = args.replace(/k/gi, "") * 1000;
-else if (args.endsWith("K")) args = args.replace(/K/gi, "") * 1000;
-else if (args.endsWith("M")) args = args.replace(/M/gi, "") * 1000000;
-     let args2 = parseInt(args)
-    let tax = Math.floor(args2 * (20) / (19) + (1))
-    let tax2 = Math.floor(args2 * (20) / (19) + (1) - (args2))
-    let tax3 = Math.floor(tax2 * (20) / (19) + (1))
-    let tax4 = Math.floor(tax2 + tax3 + args2)
-    
-    let embed = new MessageEmbed()
-      
-.setAuthor(`TAX CALCULATOR`, client.user.avatarURL({ dynamic: true }))
-
-   .setThumbnail(client.user.avatarURL({ dynamic: true }))   
-      
-.addFields([
-    {
-    name: `الضريبه : `,
-    value: `${tax}`
-},
-   {
-    name: `ضريبة الوسيط : `, 
-    value: `${tax4}`
-  }
-])
-        .setColor(message.guild.me.displayColor)
-  .setTimestamp()
-    
-   message.reply({embeds: [embed]}).catch((err) => {
-   console.log(err.message)
-   });    
-  }
-}); 
-
-// ========================================================== \\
+client.login("");
